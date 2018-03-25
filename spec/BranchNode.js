d567a88f1907d3ec993bf51e68d93c9bc0c97440
Bob Gray <gray.bob98@gmail.com>
2018-03-25 14:13:29 -0500

    adding tests for BranchNode

---
+++ spec/BranchNode.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const BranchNode = require("../src/BranchNode"),
+	Request = require("../src/Request"),
+	httpMocks = require("node-mocks-http");
+
+describe("BranchNode", () => {
+	var branchNode;
+
+	beforeEach(() => {
+		branchNode = new BranchNode({
+			path: "{name}"
+		});
+	});
+
+	it("should test true if path is entire request url", () => {
+		const request = createRequest({
+			url: "/bob"
+		});
+
+		expect(branchNode.test(request)).toBe(true);
+	});
+
+	it("should test true if path has a leading slash", () => {
+		const request = createRequest({
+				url: "/bob"
+			}),
+			branchNode = new BranchNode({
+				path: "/{name}"
+			});;
+
+		expect(branchNode.test(request)).toBe(true);
+	});
+
+	it("should parse path params when transitioning request", () => {
+		const request = createRequest({
+			url: "/bob"
+		});
+
+		branchNode.transition(request);
+
+		expect(request.getPathParam("name")).toBe("bob");
+	});
+
+	it("should test and transition properly - one node after another", () => {
+		const items = new BranchNode({
+				path: "items"
+			}),
+			item = new BranchNode({
+				path: "{id}"
+			}),
+			request = createRequest({
+				url: "/items/32"
+			});
+
+		expect(items.test(request)).toBe(true);
+		items.transition(request);
+
+		expect(item.test(request)).toBe(true);
+		item.transition(request);
+
+		expect(request.getPathParam("id")).toBe("32");
+	});
+});
+
+function createRequest (options) {
+	var incomingMessage = httpMocks.createRequest(options);
+
+	incomingMessage.setEncoding = Function.prototype;
+
+	return new Request(incomingMessage);
+}

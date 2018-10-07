eaa585fb15b9e3e3a8d5d865f05d547719d118c1
Bob Gray <gray.bob98@gmail.com>
2018-10-07 01:43:29 -0500

    fixing resolving children

--- src/ChildrenResolver.js
+++ src/ChildrenResolver.js
@@ -5,16 +5,15 @@ const HandlersResolver = require("./HandlersResolver");
 class ChildrenResolver extends HandlersResolver {
 	constructor () {
 		super();
-
 		this.resolved = this.resolved.then(this.proxy(resolveChildren));
 	}
 }
 
 function resolveChildren () {
-	return Promise.all([
-		this.branches.map(resolve),
-		this.leaves.map(resolve)
-	]);
+	return Promise.all(
+		this.branches.map(resolve)
+			.concat(this.leaves.map(resolve))
+	);
 }
 
 function resolve (child) {

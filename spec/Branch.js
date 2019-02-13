4c67cf688725211b5a2ec1efab3379fdfdafe40a
Bob Gray <gray.bob98@gmail.com>
2019-02-12 23:01:32 -0600

    adding tests to coverage recent api improvements

--- spec/Branch.js
+++ spec/Branch.js
@@ -24,12 +24,24 @@ describe("Branch", () => {
 		expect(branch.node.leaves.length).toBe(0);
 	});
 
-	it("should create a child new branch with at()", () => {
+	it("should create a new child branch with at(path)", () => {
 		const foo = branch.at("foo");
 
 		expect(foo instanceof Branch).toBe(true);
 		expect(foo).not.toBe(branch);
 		expect(branch.node.branches.length).toBe(1);
+	});
+
+	it("should add handler to new child branch with at(path, handler)", () => {
+		const foo = branch.at("foo", jasmine.createSpy());
+
+		expect(foo.node.handlers.length).toBe(1);
+	});
+
+	it("should add handlers to new child branch with at(path, ...handlers)", () => {
+		const foo = branch.at("foo", jasmine.createSpy(), jasmine.createSpy);
+
+		expect(foo.node.handlers.length).toBe(2);
 	});
 
 	it("should create a new leaf with on(method)", () => {
@@ -47,5 +59,23 @@ describe("Branch", () => {
 
 		expect(leaf instanceof Leaf).toBe(true);
 		expect(branch.node.leaves.length).toBe(1);
+	});
+
+	it("should return branch when handlers are passed to on()", () => {
+		const result = branch.on("PATCH", jasmine.createSpy("handler"));
+
+		expect(result).toBe(branch);
+		expect(branch.node.leaves.length).toBe(1);
+	});
+
+	it("should add any number of handlers to leaf using on()", () => {
+		const result = branch.on(
+			"GET",
+			jasmine.createSpy("handler"),
+			jasmine.createSpy("handler")
+		);
+
+		expect(result).toBe(branch);
+		expect(branch.node.leaves[0].handlers.length).toBe(2);
 	});
 });
